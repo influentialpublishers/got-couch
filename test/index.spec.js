3f77115d9a7ed7945e6e23d9e72114695a92919a
Nathan Sculli <scull7@gmail.com>
2016-11-08 16:50:08 -0800

    Add find and index methods
    - unify command type url generation
    - add find method
    - add addIndex method
    - add query alias for the find method.

--- test/index.spec.js
+++ test/index.spec.js
@@ -108,3 +108,94 @@ test('::list should list all of the documents in the ' +
   })
 
 )
+
+
+test('::find should query the database using the /{db}/_find endpoint',
+t =>
+
+  Promise.all([
+
+    couchdb.create(DB_NAME, 'find-foo1', { test: 'FIND', moo: 'Elsie' })
+  , couchdb.create(DB_NAME, 'find-foo2', { test: 'FIND', moo: 'Clara' })
+  , couchdb.create(DB_NAME, 'find-foo3', { test: 'FIND', moo: 'Bessie' })
+  , couchdb.create(DB_NAME, 'find-foo4', { test: 'FIND', moo: 'Babe' })
+
+  ])
+
+
+  .then(() => couchdb.find(DB_NAME, {
+
+    selector: { test: "FIND", moo: "Clara" }
+
+  }, {}))
+
+  .then((result) => {
+
+    const expected_count = 1
+
+    const expected_rows = [
+      { id: 'find-foo2', test: 'FIND', moo: 'Clara' }
+    ]
+
+    const actual_rows = result
+      .body
+      .docs
+      .map((x) => ({ id: x._id, test: x.test, moo: x.moo }))
+
+    const has_warning = result.body.warning
+
+    t.is(actual_rows.length, expected_count, 'list count is not 1')
+    t.deepEqual(actual_rows, expected_rows, 'list not what I expected')
+    t.truthy(has_warning, 'query should generate an index warning')
+
+  })
+
+)
+
+
+test('::addIndex should create an index that is used when a query is ' +
+'executed by the ::query (find) function', t =>
+
+  Promise.all([
+
+    couchdb.create(DB_NAME, 'index1', { test: 'INDEX', moo: 'Elsie' })
+  , couchdb.create(DB_NAME, 'index2', { test: 'INDEX', moo: 'Clara' })
+  , couchdb.create(DB_NAME, 'index3', { test: 'INDEX', moo: 'Bessie' })
+  , couchdb.create(DB_NAME, 'index4', { test: 'INDEX', moo: 'Babe' })
+
+  ])
+
+
+  .then(() => couchdb.addIndex(DB_NAME, {
+    index: { fields: ["test", "moo"] }
+  , name: "test-moo-index"
+  }, {}))
+
+
+  .then(() => couchdb.query(DB_NAME, {
+    selector: { test: 'INDEX', moo: 'Babe' }
+  }, {}))
+
+  .then((result) => {
+
+    const expected_count = 1
+
+    const expected_rows = [
+      { id: 'index4', test: 'INDEX', moo: 'Babe' }
+    ]
+
+    const actual_rows = result
+      .body
+      .docs
+      .map((x) => ({ id: x._id, test: x.test, moo: x.moo }))
+
+
+    const has_warning = result.body.hasOwnProperty('warning')
+
+    t.is(actual_rows.length, expected_count, 'list count is not 1')
+    t.deepEqual(actual_rows, expected_rows, 'list not what I expected')
+    t.falsy(has_warning, 'query generated an unexpected warning message')
+
+  })
+
+)
