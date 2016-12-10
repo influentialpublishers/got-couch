827a5a171d87445cd2a597dbafadee76f1f7d15d
mszymulanski <mszymulanski@gmail.com>
2016-12-09 16:41:13 -0800

    Add error catch in the base test.

--- test/_test-base.js
+++ test/_test-base.js
@@ -9,15 +9,24 @@ const initTest = (couchdb, db_name) => {
 
   test.before(() => couchdb.then(
     (connection) => connection.db.delete(db_name).catch(() => null)
-  ))
+  ).catch((err) => {
+    console.log(err)
+    throw err
+  }))
 
   test.before(() => couchdb.then(
     (connection) => connection.db.create(db_name)
-  ))
+  ).catch((err) => {
+    console.log(err)
+    throw err
+  }))
 
   test.after(() => couchdb.then(
     (connection) => connection.db.delete(db_name)
-  ))
+  ).catch((err) => {
+    console.log(err)
+    throw err
+  }))
 
   return test
 }
